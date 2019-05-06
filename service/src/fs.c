ae27aace2397dc29515318261a1033083855a711
slipper <slipper@0ops.net>
2019-05-06 11:34:27 +0800

    trim file path

--- service/src/fs.c
+++ service/src/fs.c
@@ -6,7 +6,13 @@
 static int file_cnt = 0;
 static file_t files[MAX_FILE_COUNT];
 
+static const char *trim_path(const char *path) {
+    while (*path == '/' || *path == '.') path++;
+    return path;
+}
+
 file_t *query_file(const char *name) {
+    name = trim_path(name);
     for (int i = 0; i < file_cnt; i++) {
         if (files[i].fd != -1 && !strcmp(files[i].name, name)) {
             return &files[i];
@@ -17,7 +23,7 @@ file_t *query_file(const char *name) {
 
 file_t *open_file(const char *name, int perm) {
     file_t file;
-    strncpy(file.name, name, sizeof(file.name));
+    strncpy(file.name, trim_path(name), sizeof(file.name));
     file_t *f = query_file(file.name);
     if (f != NULL) {
         if (perm & f->perm) {
@@ -25,9 +31,6 @@ file_t *open_file(const char *name, int perm) {
         } else {
             return NULL;
         }
-    }
-    if (strchr(file.name, '/') || strchr(file.name, '.')) {
-        return NULL;
     }
     file.fd = memfd_create(file.name, 0);
     file.perm = FILE_RDWR;
@@ -41,7 +44,7 @@ file_t *open_file(const char *name, int perm) {
 
 void append_file(const char *name, int fd, int perm) {
     if (fd != -1 && file_cnt < MAX_FILE_COUNT) {
-        strncpy(files[file_cnt].name, name, sizeof(files[file_cnt].name));
+        strncpy(files[file_cnt].name, trim_path(name), sizeof(files[file_cnt].name));
         files[file_cnt].perm = perm;
         files[file_cnt++].fd = fd;
     }
