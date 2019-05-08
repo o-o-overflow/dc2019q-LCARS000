ba177098295c7b6c730016ec3baee394461ac77c
slipper <slipper@0ops.net>
2019-05-08 12:39:53 +0800

    correct filenames
    *.sys: system services, unsigned raw binary
    *.app: user apps, unsigned raw binary
    *.uapp: user apps, unsigned/encrypted packed binary
    *.papp: platform apps, signed/encrypted packed binary
    *.sapp: system apps, signed/encrypted packed binary

--- service/src/x.py
+++ service/src/x.py
@@ -1,22 +1,17 @@
 from pwn import *
 
 r = process(['./mon', './init.sys', './loader.sys', './echo.sys',
-    './crypto.sys', './test.app', 'root.key'])
-# r = process(['strace', '-f', './mon', './io.sys', './loader.sys', './echo.sys'])
+    './crypto.sys', './svc.uapp', 'root.key'])
 
-def download(rfile, lfile):
-    with open(lfile) as f:
+def download(app):
+    with open(app) as f:
         blob = f.read()
-        r.sendline('download %s %d' % (rfile, len(blob)))
+        r.sendline('download %s %d' % (app, len(blob)))
         r.send(blob)
 
-# download('test.app', 'test.app')
-# r.sendline('run test.app')
-download('untrusted.app', 'untrusted.app')
-download('platform.app', 'platform.app')
-download('system.app', 'system.app')
-# r.sendline('run untrusted.app')
-# r.sendline('run platform.app')
-r.sendline('run system.app')
+download('perm.uapp')
+download('perm.papp')
+download('perm.sapp')
+r.sendline('run perm.sapp')
 
 r.interactive()
