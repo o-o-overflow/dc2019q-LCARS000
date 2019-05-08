fd773173aa1101af4b4ce0159a69a6588effe902
slipper <slipper@0ops.net>
2019-05-08 11:22:50 +0800

    fix __memset_chk prototype

--- service/src/gnu-c.c
+++ service/src/gnu-c.c
@@ -21,6 +21,6 @@ void *__memcpy_chk(void *dst, const void *src, size_t size, size_t max_size) {
     return memcpy(dst, src, size);
 }
 
-void *__memset_chk(void *s, char c, size_t n, size_t max_size) {
+void *__memset_chk(void *s, int c, size_t n, size_t max_size) {
     return memset(s, c, n);
 }
--- service/src/gnu-c.h
+++ service/src/gnu-c.h
@@ -11,5 +11,5 @@ void free(void *ptr);
 void *realloc(void *ptr, size_t sz);
 
 void *__memcpy_chk(void *dst, const void *src, size_t size, size_t max_size);
-void *__memset_chk(void *s, char c, size_t n, size_t max_size);
+void *__memset_chk(void *s, int c, size_t n, size_t max_size);
 #endif
